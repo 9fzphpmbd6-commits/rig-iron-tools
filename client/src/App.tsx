2e2a0056b4a097dc730173ceb88be934cdb0189f
9fzphpmbd6-commits <9fzphpmbd6@privaterelay.appleid.com>
2026-03-20 16:52:49 -0500

    Switch from hash routing to browser history routing for SEO

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route, Router } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
+
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -53,7 +53,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router hook={useHashLocation}>
+        <Router>
           <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1">
